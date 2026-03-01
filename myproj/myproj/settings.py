f19f56244ba3fdaadda342b38ddc3333cde11e65
mohit <mohitsh7979@gmail.com>
2026-03-01 12:12:00 +0530

    implement curd api

--- myproj/myproj/settings.py
+++ myproj/myproj/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'task_app',
+    'auth_app'
 ]
 
 MIDDLEWARE = [
